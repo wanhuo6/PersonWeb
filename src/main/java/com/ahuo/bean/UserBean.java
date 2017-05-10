104b994dae7e811e3a925186d5e00e72ede7a99b
liuhuijie <edward_hs@163.com>
2017-05-10 11:18:00 +0800

    first_commit

---
+++ src/main/java/com/ahuo/bean/UserBean.java
@@ -0,0 +1,10 @@
+package com.ahuo.bean;
+
+/**
+ * Created by wanhuo on 2017-5-10.
+ */
+public class UserBean {
+    public String name;
+    public Integer age;
+    public Double height;
+}
---
+++ src/main/java/com/ahuo/servlet/LoginServlet.java
@@ -0,0 +1,33 @@
+package com.ahuo.servlet;
+
+import com.ahuo.bean.UserBean;
+import com.alibaba.fastjson.JSONObject;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by ahuo on 17-5-10.
+ */
+@WebServlet(name = "LoginServlet",urlPatterns = "/GetLogin")
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        UserBean userBean=new UserBean();
+        userBean.name="lin";
+        userBean.age=12;
+        userBean.height=175.0;
+        String jsonObjectStr= JSONObject.toJSONString(userBean);
+
+        PrintWriter printWriter= resp.getWriter();
+        printWriter.write(jsonObjectStr);
+        resp.setContentType("text/json");
+        resp.setCharacterEncoding("gb2312");
+    }
+}
